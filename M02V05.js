b7ff165be82c6323d03b5444eaa91a0950addf1c
Sneara0 <snearaparvin.cse@gmail.com>
2025-11-03 23:05:02 +0600

    M02V05.JS

--- M02V05.js
+++ M02V05.js
@@ -2,15 +2,31 @@
 
 const  cartItems=[
     { id: 1, name: 'Item A', price: 10.0, quantity: 2 },
-    { id: 2, name: 'Item B', price: 15.0, quantity: 1 },
+    { id: 2, name: 'Item B', price: 15.0, quantity: 4 },
     { id: 3, name: 'Item C', price: 7.5, quantity: 3 },
 
-]
+];
+
+const subtotal= cartItems.reduce((subtotal,product)=>{
+    console.log(subtotal,product);
+    return subtotal + product.price * product.quantity;
+},0);
+//console.log(subtotal)
 
 //find best scorer
 const players=[
     {name:"Jamal Bhuyan",score:50},
     {name:"Sheikh Mursalin",score:80},
     {name:"Rakib Hossain",score:65},
-    
+
 ];
+
+const bestplayer=players.reduce((bestplayer,player)=>{
+    console.log(bestplayer,player);
+    if (bestplayer.score>player.score){
+        return bestplayer;
+    }
+    return player;
+
+},players[0]);
+console.log(bestplayer);
