e2868cf102d047895aaa711e5d678c14bd25ef03
Sneara0 <snearaparvin.cse@gmail.com>
2025-11-07 11:15:46 +0600

    add

--- M03V07.js
+++ M03V07.js
@@ -28,24 +28,75 @@ class LinkedList{
 
     }
 
-    prepend(){
+    prepend( value){
+
+         const newNode= new node(value);
+
+        if( this.head === null){
+            this.head=newNode
+            this.tail=newNode
+
+        } else{
+            newNode.next=this.head
+            this.head=newNode
+           
+            
+        }
+        this.length++;
 
     }
+
+
+
+    insert(index,value){
+        if(index<0 || index> this.length){
+            console.error("index out of bound:sohoz Bangla Vasai muri kha")
+            return undefined;
+        }
+        if (index===0){
+            return this.prepend(value);
+        }
+
+
+
+
+        if(index=== this.length){
+            return this.appened(value)
+        }
+        //if the insert is in the middle
+        // find leading node
+
+        let count=0;
+        let leadingNode= this.head;
+
+        while ( count !== index-1){
+            leadingNode=leadingNode.next
+            count++
+        }
+
+        console.log(leadingNode)
+
+        
+    }
+
 
     remove(){
 
     }
-
+    
     print(){
+        const arr=[]
         let currentNode= this.head;
 
         while(currentNode !== null){
-            console.log (currentNode.value);
+            arr.push (currentNode.value);
             currentNode= currentNode.next
 
 
 
         }
+
+        console.log(arr.join("→"),"→null")
 
     }
 }
@@ -54,4 +105,8 @@ const List = new LinkedList ();
 List.appened(1);
 List.appened(2);
 List.appened(3);
+List.prepend(30);
+List.prepend(20);
+List.prepend(10);
+List.insert(2,100);
 List.print()
